f7f9b2256ad7f222d8dce5c6bd7c069cca77fd5c
semund <semund.sigurdsson@gmail.com>
2022-06-24 17:16:03 +0300

    Finished the layout of registration page

--- hotel/registration/forms.py
+++ hotel/registration/forms.py
@@ -1,5 +1,22 @@
 from django import forms
 
+from .models import Booking, Guest, Room
 
-class GuestCheckin(forms.ModelForm):
-    pass
+
+class GuestForm(forms.ModelForm):
+    class Meta:
+        model = Guest
+        fields = ('passport_number', 'date_of_issue', 'first_name', 'last_name',
+                  'additional_name', 'birth_date', 'phone', 'email')
+
+
+class BookingForm(forms.ModelForm):
+    class Meta:
+        model = Booking
+        fields = ('checkin_date', 'checkout_date')
+
+
+class RoomForm(forms.ModelForm):
+    class Meta:
+        model = Room
+        fields = ('number',)
--- hotel/registration/views.py
+++ hotel/registration/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
 
+from .forms import GuestForm, BookingForm, RoomForm
+from .models import Room
+
 
 # Create your views here.
 def checkin(request):
-    return render(request, 'registration/checkin.html')
+    room_list = Room.objects.all()
+    form_guest = GuestForm()
+    form_booking = BookingForm()
+    form_room = RoomForm()
+
+    context = {
+        'page_title': 'Checkin',
+        'room_list': room_list,
+        'form_guest': form_guest,
+        'form_booking': form_booking,
+        'form_room': form_room
+    }
+
+    return render(request, 'registration/checkin.html', context=context)
