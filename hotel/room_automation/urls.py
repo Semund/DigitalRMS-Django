3b5aa9a4b5b8d7479a6faf498554fda432d1f2fe
semund <semund.sigurdsson@gmail.com>
2022-07-01 23:46:14 +0300

    Refactoring room-automation app

--- hotel/room_automation/urls.py
+++ hotel/room_automation/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from room_automation.views import room_view
+from room_automation.views import room_view, RoomAutomationView
 
 urlpatterns = [
     path('', room_view, name='room'),
+    path('test/', RoomAutomationView.as_view(), name='room_test')
 ]
--- hotel/room_automation/views.py
+++ hotel/room_automation/views.py
@@ -4,6 +4,8 @@ from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+from django.views.generic import TemplateView
+
 from registration.models import Room
 from room_automation.forms import RoomAutomationForm
 from room_automation.models import RoomAutomation
@@ -29,3 +31,33 @@ def room_view(request):
         return JsonResponse(data=room_all_params)
 
     return render(request, 'room_automation/room_automation.html', context=context)
+
+
+class RoomAutomationView(TemplateView):
+    template_name = 'room_automation/room_test.html'
+    room_select_class = RoomAutomationForm
+
+    def post(self, request, *args, **kwargs):
+        post_data = request.POST or None
+        room_select_form = self.room_select_class(post_data)
+        context = self.get_context_data(room_select_form=room_select_form)
+        if room_select_form.is_valid():
+            self.collect_room_json_data(room_id=post_data['room'])
+
+        return self.render_to_response(context)
+
+    def get(self, request, *args, **kwargs):
+        return self.post(request, *args, **kwargs)
+
+    def collect_room_json_data(self, room_id):
+        try:
+            room_parameters = RoomAutomation.objects.get(pk=int(room_id))
+        except (ValueError,):
+            room_parameters = None  # TODO error handling
+
+        return JsonResponse(data={
+            'room_number': room_parameters.room.number,
+            'room_light_parameters': json.loads(room_parameters.light),
+            'room_climat_parameters': json.loads(room_parameters.climat)
+        })
+
