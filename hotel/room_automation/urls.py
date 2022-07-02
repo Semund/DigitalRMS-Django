370be4df8548b0d8afae0b40d5d2891984225e9d
Semyon Fedorov <semund.sigurdsson@gmail.com>
2022-07-02 11:52:13 +0300

    Completed refactoring a room-automation app

--- hotel/room_automation/urls.py
+++ hotel/room_automation/urls.py
@@ -1,8 +1,7 @@
 from django.urls import path
 
-from room_automation.views import room_view, RoomAutomationView
+from room_automation.views import room_view
 
 urlpatterns = [
     path('', room_view, name='room'),
-    path('test/', RoomAutomationView.as_view(), name='room_test')
 ]
--- hotel/room_automation/views.py
+++ hotel/room_automation/views.py
@@ -3,12 +3,24 @@ import json
 from django.http import JsonResponse
 from django.shortcuts import render
 
-# Create your views here.
-from django.views.generic import TemplateView
-
-from registration.models import Room
 from room_automation.forms import RoomAutomationForm
 from room_automation.models import RoomAutomation
+
+
+# Create your views here.
+
+
+def collect_room_automation_data(room_id: int) -> dict:
+    room_automation_object = RoomAutomation.objects.get(room_id=room_id)
+    room_number = room_automation_object.room.number
+    room_light_params = json.loads(room_automation_object.light)
+    room_climat_params = json.loads(room_automation_object.climat)
+    room_automation_data = {
+        'room_number': room_number,
+        'room_light': room_light_params,
+        'room_climat': room_climat_params
+    }
+    return room_automation_data
 
 
 def room_view(request):
@@ -18,46 +30,7 @@ def room_view(request):
 
     if request.method == 'POST':
         room_id = int(request.POST['room'])
-        room_number = Room.objects.get(pk=room_id).number
-        room_automation = RoomAutomation.objects.get(pk=room_id)
-        room_light = json.loads(room_automation.light)
-        room_climat = json.loads(room_automation.climat)
-        room_all_params = {
-            'room_number': room_number,
-            'room_light': room_light,
-            'room_climat': room_climat
-        }
-
-        return JsonResponse(data=room_all_params)
+        room_automation_data = collect_room_automation_data(room_id)
+        return JsonResponse(data=room_automation_data)
 
     return render(request, 'room_automation/room_automation.html', context=context)
-
-
-class RoomAutomationView(TemplateView):
-    template_name = 'room_automation/room_test.html'
-    room_select_class = RoomAutomationForm
-
-    def post(self, request, *args, **kwargs):
-        post_data = request.POST or None
-        room_select_form = self.room_select_class(post_data)
-        context = self.get_context_data(room_select_form=room_select_form)
-        if room_select_form.is_valid():
-            self.collect_room_json_data(room_id=post_data['room'])
-
-        return self.render_to_response(context)
-
-    def get(self, request, *args, **kwargs):
-        return self.post(request, *args, **kwargs)
-
-    def collect_room_json_data(self, room_id):
-        try:
-            room_parameters = RoomAutomation.objects.get(pk=int(room_id))
-        except (ValueError,):
-            room_parameters = None  # TODO error handling
-
-        return JsonResponse(data={
-            'room_number': room_parameters.room.number,
-            'room_light_parameters': json.loads(room_parameters.light),
-            'room_climat_parameters': json.loads(room_parameters.climat)
-        })
-
